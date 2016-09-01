70624b6a0cd1d677cb56db550e3d06cef18367e1
Chris Mytton <chrismytton@gmail.com>
2016-09-01 16:18:42 +0100

    Check the UUID field actually contains a UUID

--- test/combined_areas_test.rb
+++ test/combined_areas_test.rb
@@ -3,11 +3,12 @@ require_relative '../lib/combined_areas'
 
 describe CombinedAreas do
   subject { CombinedAreas.new }
+  let(:uuid_regex) { /^\w{8}-(\w{4}-){3}\w{12}$/ }
 
   it 'adds a single area' do
     subject.add_area('wikidata-areas', id: 'Q3296251', name: 'Anvard')
     out = subject.as_json.first
-    out[:uuid].wont_be_nil
+    out[:uuid].must_match uuid_regex
     out[:type].must_equal 'wikidata-areas'
     out[:name].must_equal 'Anvard'
     out[:id].must_equal 'Q3296251'
