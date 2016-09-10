271bf2b011c94a9977905d8ef2cf1bafc6e399d1
Tony Bowden <tony@tmtm.com>
2016-09-10 20:09:29 +0100

    extract a Legislature::Metadata class
    cut-n-paste edition

--- lib/country_data.rb
+++ lib/country_data.rb
@@ -16,31 +16,7 @@ module EveryPolitician
           country:      name,
           code:         meta['iso_code'].upcase,
           slug:         slug,
-          legislatures: dirs.map do |h|
-            json_file = h + '/ep-popolo-v1.0.json'
-            name_file = h + '/names.csv'
-            remote_source = 'https://cdn.rawgit.com/everypolitician/everypolitician-data/%s/%s'
-            popolo, statement_count = json_from(json_file)
-            sha, lastmod = commit_metadata[json_file].values_at :sha, :timestamp
-            lname = name_from(popolo)
-            lslug = h.split('/').last.tr('_', '-')
-            {
-              name:                lname,
-              slug:                lslug,
-              sources_directory:   "#{h}/sources",
-              popolo:              json_file,
-              popolo_url:          remote_source % [sha, json_file],
-              names:               name_file,
-              lastmod:             lastmod,
-              person_count:        popolo[:persons].size,
-              sha:                 sha,
-              legislative_periods: terms_from(popolo, h).each do |t|
-                term_csv_sha = commit_metadata[t[:csv]][:sha]
-                t[:csv_url] = remote_source % [term_csv_sha, t[:csv]]
-              end,
-              statement_count:     statement_count,
-            }
-          end,
+          legislatures: dirs.map { |h| Legislature::Metadata.new(dir: h, commit_metadata: commit_metadata).stanza },
         }
       end
 
@@ -67,7 +43,45 @@ module EveryPolitician
       def slug
         country.tr('_', '-')
       end
+    end
+  end
 
+  class Legislature
+    class Metadata
+      def initialize(dir:, commit_metadata:)
+        @dir = dir
+        @commit_metadata = commit_metadata
+      end
+
+      def stanza
+        json_file = dir + '/ep-popolo-v1.0.json'
+        name_file = dir + '/names.csv'
+        remote_source = 'https://cdn.rawgit.com/everypolitician/everypolitician-data/%s/%s'
+        popolo, statement_count = json_from(json_file)
+        sha, lastmod = commit_metadata[json_file].values_at :sha, :timestamp
+        lname = name_from(popolo)
+        lslug = dir.split('/').last.tr('_', '-')
+        {
+          name:                lname,
+          slug:                lslug,
+          sources_directory:   "#{dir}/sources",
+          popolo:              json_file,
+          popolo_url:          remote_source % [sha, json_file],
+          names:               name_file,
+          lastmod:             lastmod,
+          person_count:        popolo[:persons].size,
+          sha:                 sha,
+          legislative_periods: terms_from(popolo, dir).each do |t|
+            term_csv_sha = commit_metadata[t[:csv]][:sha]
+            t[:csv_url] = remote_source % [term_csv_sha, t[:csv]]
+          end,
+          statement_count:     statement_count,
+        }
+      end
+
+      private
+
+      attr_reader :dir, :commit_metadata
     end
   end
 end
