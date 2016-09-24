f7980b11f54f3d9dec760424a5a8e8565c5989ad
Tony Bowden <tony@tmtm.com>
2016-09-24 20:31:56 +0100

    extract OCD source class
    Split Source::OCD out into its own file

--- lib/source.rb
+++ lib/source.rb
@@ -98,26 +98,6 @@ module Source
     end
   end
 
-  class OCD < CSV
-    def fields
-      %i(area area_id)
-    end
-
-    def fuzzy_match?
-      i(:merge)[:fuzzy]
-    end
-
-    def overrides
-      return {} unless i(:merge)
-      return {} unless i(:merge).key? :overrides
-      i(:merge)[:overrides]
-    end
-
-    def generate
-      i(:generate)
-    end
-  end
-
   class Term < PlainCSV
   end
 
