9bbeb2766f3522c63c5f940f70f81e74e1c0caa5
Tony Bowden <tony@tmtm.com>
2016-09-24 20:33:53 +0100

    extract CSV source class
    Split Source::CSV out into its own file

--- lib/source.rb
+++ lib/source.rb
@@ -88,22 +88,6 @@ module Source
     end
   end
 
-  class CSV < PlainCSV
-    def fields
-      headers.map { |h| remap(h.to_s.downcase) }
-    end
-
-    def raw_table
-      rows = []
-      super.each do |row|
-        # Need to make a copy in case there are multiple source columns
-        # mapping to the same term (e.g. with areas)
-        rows << Hash[row.keys.each.map { |h| [remap(h), row[h].nil? ? nil : row[h].tidy] }]
-      end
-      rows
-    end
-  end
-
   class JSON < Base
     def fields
       []
