72167a0a4cced3530a942cfd510eea40c273e8cc
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-10-09 20:49:12 +0100

    Use the new remap method in the rake task

--- rake_config/memberships.rb
+++ rake_config/memberships.rb
@@ -3,19 +3,7 @@ desc 'Remap memberships ids'
 namespace :memberships do
   task :remap_ids, [:from, :to] do |_, args|
     @INSTRUCTIONS.sources_of_type('membership').each do |source|
-      file = source.mapfile
-      data = remap(file, args)
-      file.rewrite(data)
+      source.mapfile.remap(args[:from], args[:to])
     end
   end
-end
-
-private
-
-def remap(file, args)
-  data = file.mapping
-  abort "No existing data for #{args[:from]}" unless data[args[:from]]
-  abort "Already have data for #{args[:to]}" if data[args[:to]]
-  data[args[:to]] = data.delete(args[:from])
-  data
 end
