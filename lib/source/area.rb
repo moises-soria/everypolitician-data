18262257b43ebb2c28812779cc5f76d7659f12dc
Tony Bowden <tony@tmtm.com>
2016-12-14 16:13:07 +0000

    extract a Source::Area::Row class
    This lets us re-process each row of the source data into the format we
    want in a simpler and cleaner way.

--- lib/source/area.rb
+++ lib/source/area.rb
@@ -1,31 +1,58 @@
 require_relative 'plain_csv'
+require 'field_serializer'
 
 module Source
   class Area < PlainCSV
+    class Row
+      include FieldSerializer
+
+      def initialize(r, reconciliation_data)
+        @area = r
+        @reconciliation_data = reconciliation_data
+      end
+
+      field :id do
+        reconciliation_data[area[:id]]
+      end
+
+      field :identifiers do
+        [
+          {
+            identifier: area[:id],
+            scheme:     'wikidata',
+          },
+        ]
+      end
+
+      field :other_names do
+        area.keys.select { |k| k.to_s.start_with? 'name__' }.map do |k|
+          {
+            lang: k.to_s[/name__(\w+)/, 1],
+            name: area[k],
+            note: 'multilingual',
+            # TODO: credit the source
+            # source: 'wikidata',
+          }
+        end
+      end
+
+      private
+
+      attr_reader :area, :reconciliation_data
+    end
+
     def to_popolo
       {
-        areas: as_table.map do |area|
-          {
-            id:          reconciliation_data[area[:id]],
-            identifiers: [{
-              identifier: area[:id],
-              scheme:     'wikidata',
-            },],
-            other_names: area.keys.select { |k| k.to_s.start_with? 'name__' }.map do |k|
-              {
-                lang: k.to_s[/name__(\w+)/, 1],
-                name: area[k],
-                note: 'multilingual',
-                # TODO: credit the source
-                # source: 'wikidata',
-              }
-            end,
-          }
-        end.reject { |a| a[:id].nil? },
+        areas: area_data,
       }
     end
 
     private
+
+    def area_data
+      as_table.map { |area| Row.new(area, reconciliation_data).to_h }
+              .reject { |a| a[:id].nil? }
+    end
 
     def reconciliation_file
       'sources/' + i(:merge)[:reconciliation_file]
