f5de872178f44ff12e2e44aa1323187276b1c96a
Tony Bowden <tony@tmtm.com>
2017-01-13 11:28:54 +0000

    Report on Cabinet memberships not posts
    Change the stats reporting to list the number of known Cabinet
    memberships not just roles — i.e. so that it goes up when we know a new
    person becoming Minister of Defence, not just when we add a new position.

--- rake_build/generate_stats.rb
+++ rake_build/generate_stats.rb
@@ -7,7 +7,7 @@ STATSFILE = Pathname.new('unstable/stats.json')
 namespace :stats do
   task regenerate: 'ep-popolo-v1.0.json' do
     popolo = Everypolitician::Popolo.read('ep-popolo-v1.0.json')
-    stats = StatsFile.new(popolo: popolo, position_filter: POSITION_FILTER).stats
+    stats = StatsFile.new(popolo: popolo, position_file: POSITION_CSV).stats
 
     STATSFILE.dirname.mkpath
     STATSFILE.write(JSON.pretty_generate(stats))
