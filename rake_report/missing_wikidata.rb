a5b9d5d6deb2baa351d94badcb79d8195e6e1dc1
Tony Bowden <tony@tmtm.com>
2017-01-25 09:28:52 +0000

    Add rake report on missing Wikidata members
    Add a rake task (rake report:missing_wikidata) to tell us which members
    of the latest term aren't yet matched to Wikidata.
    
    This will make it easier to ensure that at least the current information
    is as well matched as possible.

---
+++ rake_report/missing_wikidata.rb
@@ -0,0 +1,8 @@
+namespace :report do
+  task :missing_wikidata do
+    popolo = Everypolitician::Popolo.read('ep-popolo-v1.0.json')
+    popolo.latest_term.memberships.map(&:person).reject(&:wikidata).sort_by(&:name).group_by(&:id).each do |id, ps|
+      puts "%s (%s)" % [ps.first.name, id]
+    end
+  end
+end
