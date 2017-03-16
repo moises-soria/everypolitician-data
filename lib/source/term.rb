7b55d76f713ab6458cd4eeb3b7a15a8002fd809a
Tony Bowden <tony@tmtm.com>
2017-03-16 15:17:13 +0000

    Complain about overlapping terms
    I wasn't 100% sure whether to do this as a separate check step of the
    build, but I think this is a significant enough problem to justify it
    essentially being an "this file isn't valid"-level error as soon as you
    try to read a terms file.

--- lib/source/term.rb
+++ lib/source/term.rb
@@ -3,13 +3,24 @@ require_relative 'plain_csv'
 module Source
   class Term < PlainCSV
     def to_popolo
+      oe = overlapping_events
+      raise "Overlapping terms: #{oe.map { |ts| "#{ts.first[:id]} -> #{ts.last[:id]}" }}" unless oe.empty?
+
       { events: events }
     end
 
     private
 
+    def overlapping_events
+      sorted_events.each_cons(2).select { |e, l| l[:start_date] < e[:end_date] }
+    end
+
+    def sorted_events
+      events.sort_by { |e| e[:start_date] }
+    end
+
     def events
-      as_table.map do |row|
+      @events ||= as_table.map do |row|
         {
           id:              row[:id][/\//] ? row[:id] : "term/#{row[:id]}",
           name:            row[:name],
