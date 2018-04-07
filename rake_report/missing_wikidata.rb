79b7001c4439a2cb983cf9fac6c9ba403f4ed9cd
Tony Bowden <tony@tmtm.com>
2018-04-07 10:46:01 +0100

    Show source URL in missing_wikidata
    When displaying the list of current members with no Wikidata, it's handy
    to also see the source URL, to make it easier to add the person.
    
    This no longer sorts the people by name, but I don't think that was
    particularly important anyway.

--- rake_report/missing_wikidata.rb
+++ rake_report/missing_wikidata.rb
@@ -3,8 +3,8 @@
 namespace :report do
   task :missing_wikidata do
     popolo = Everypolitician::Popolo.read('ep-popolo-v1.0.json')
-    popolo.latest_term.memberships.map(&:person).reject(&:wikidata).sort_by(&:name).group_by(&:id).each do |id, ps|
-      puts '%s (%s)' % [ps.first.name, id]
+    popolo.latest_term.memberships.reject { |mem| mem.person.wikidata }.uniq(&:person).each do |mem|
+      puts '%s (%s) %s' % [mem.person.name, mem.person.id, mem.sources.first&.[](:url)]
     end
   end
 end
